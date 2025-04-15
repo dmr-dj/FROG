4d2894195c35423136719e441cfb5d7afaef0a71
dmr-dj <didier.roche@lsce.ipsl.fr>
2025-04-15 13:00:37 +0200

    Cleaned up some things in namelist reading

--- src/Parametrisation.f90
+++ src/Parametrisation.f90
@@ -5,14 +5,17 @@ module Parametrisation
 
   public !dmr here can be fully public, these are only parameter constants
 
-  character(len=100) :: namerun
+
+  integer, parameter  :: str_len = 256
+
+
+  character(len=str_len) :: namerun
   integer :: TotTime         !temps total en année
   integer :: nb_day_per_month           !
   integer :: nb_mon_per_year
   integer :: t_fin
   integer :: YearType         !nombre de jour par an
   integer :: z_num            !nombre de couches étudiée
-!dmr [UNUSED]  integer :: EXPE = 1               !de 1 à 4, quelle expérience va être réalisée
   integer :: GridType           !(1) Log-generated, (2) Linear-generated
   integer :: PorosityType       !(1) linéaire, (autre) exponentiellement décroissante en fonction de la profondeur
   integer :: Bool_Snow          ! forçage en neige ou non (1 ou 0)
@@ -74,11 +77,11 @@ module Parametrisation
   !dmr spatialisation
   integer :: gridNoMax    !dmr spatial index, no assumption of the spatial arrangement
 
-  character(len= 100) :: Tempsolinit
-  character(len= 100) :: Tempairmonth
-  character(len= 100) :: Tempairday
-  character(len= 100) :: Tempsnowmonth
-  character(len= 100) :: Tempsnowday
+  character(len=str_len) :: Tempsolinit
+  character(len=str_len) :: Tempairmonth
+  character(len=str_len) :: Tempairday
+  character(len=str_len) :: Tempsnowmonth
+  character(len=str_len) :: Tempsnowday
 
 contains
 
@@ -88,8 +91,8 @@ contains
 
 !déclaration des variables
 
-    INTEGER :: rc,fu, stderr
-    CHARACTER(len= 19)  :: file_path ="vamper_namelist.nml"
+    INTEGER :: rc,fu, stderr, fo
+    CHARACTER(len= 19)  :: file_path ="vamper_namelist.nml", file_path_chck = "vamper_namelist.chk"
     CHARACTER(len=1), PARAMETER :: tab=char(9)
     CHARACTER(len=3), PARAMETER :: spa=""//tab//tab//tab
 
@@ -113,77 +116,84 @@ contains
 
     ! Open and read Namelist file.
     OPEN (action='read', file=file_path, iostat=rc, newunit=fu)
+
     IF (rc /= 0) WRITE (stderr, '("Error: invalid Namelist format")')
     READ (nml=Param, iostat=rc, unit=fu)
-!    CLOSE (fu)
-!    OPEN (action='read', file=file_path, iostat=rc, newunit=fu)
+
     IF (rc /= 0) WRITE (stderr, '("Error: invalid Namelist format")')
     READ (nml=Physique, iostat=rc, unit=fu)
-!    CLOSE (fu)
-!    OPEN (action='read', file=file_path, iostat=rc, newunit=fu)
+
     IF (rc /= 0) WRITE (stderr, '("Error: invalid Namelist format")')
     READ (nml=Tempdata, iostat=rc, unit=fu)
+
     IF (rc /= 0) WRITE (stderr, '("Error: invalid Namelist format")')
+
     CLOSE (fu)
 
-    write(*,*) "=== PARAMETERS CHECK ==="
+    ! Write out a file to check the parameters that have been read
 
-    write(*,*) "namerun"//spa, namerun
-    write(*,*) "TotTime"//spa, TotTime         !temps total en année
-    write(*,*) "nb_day_per_month"//spa, nb_day_per_month           !nombre de jour entre chaque pas de temps
-    write(*,*) "t_fin"//spa, t_fin
-    write(*,*) "YearType"//spa, YearType         !nombre de jour par an
-    write(*,*) "z_num"//spa, z_num            !nombre de couches étudiée
-    write(*,*) "GridType"//spa, GridType           !(1) Log-generated, (2) Linear-generated
-    write(*,*) "PorosityType"//spa, PorosityType       !(1) linéaire, (autre) exponentiellement décroissante en fonction de la profondeur
-    write(*,*) "Bool_Snow"//spa, Bool_Snow          ! forçage en neige ou non (1 ou 0)
-    write(*,*) "Bool_Organic"//spa, Bool_Organic       ! prise en compte de la couche organique ou non (1 ou 0)
-    write(*,*) "EQ_Tr"//spa, EQ_Tr             ! Equilibrum run (0) or Transient run (1) -> using different forcing Temperature and snow
-    write(*,*) "EQ1_EQ2"//spa, EQ1_EQ2           ! EQ1(1) initial temperature calculated with the Geothermal heat flux. EQ2 initial temperature read in a file .txt
-    write(*,*) "Bool_delta"//spa, Bool_delta        !
-    write(*,*) "Bool_glacial"//spa, Bool_glacial          ! Using glacial index to modify air temperature
-    write(*,*) "Bool_layer_temp"//spa, Bool_layer_temp       ! Creation of .txt with the temperature of the soil at different layer
-    write(*,*) "Bool_Swe_Snw"//spa, Bool_Swe_Snw          ! (1) Snow forcing, (0) Swe forcing
-    write(*,*) "Bool_Model_Snow"//spa, Bool_Model_Snow       ! (1) Usinsg snow model to find snow_depth, (0) Forcing with snow_depth
-    write(*,*) "Bool_Bessi"//spa, Bool_Bessi
-    write(*,*) "Bool_geometric"//spa, Bool_geometric
-    write(*,*) "Depth"//spa, Depth              !profondeur de la modélisation
-    write(*,*) "T_init"//spa, T_init                !température initiale a la surface
-    write(*,*) "T_freeze"//spa, T_freeze             !température où l'eau est considérée comme gelée
-    write(*,*) "freezing_range"//spa, freezing_range          ! Temperature at wich the snow start to melt
-    write(*,*) "Gfx"//spa, Gfx                  ! flux géothermique de la terre (a modifier peut être)
-    write(*,*) "Porosity_soil"//spa, Porosity_soil       ! porosité du sol
-    write(*,*) "organic_depth"//spa, organic_depth     ! profondeur de la couche organique
-    write(*,*) "n_organic"//spa, n_organic           ! porosité de la couche organique
-    write(*,*) "n_soil_bot"//spa, n_soil_bot         ! porosité en bas de la couche de sol
-    write(*,*) "alpha"//spa, alpha
-    write(*,*) "rho_snow_freeze"//spa, rho_snow_freeze            ! densité de la neige
-    write(*,*) "rho_water"//spa, rho_water          ! densité de l'eau
-    write(*,*) "rho_ice"//spa, rho_ice             ! densité de la glace
-    write(*,*) "rho_organic"//spa, rho_organic        ! densité de la matière organique
-    write(*,*) "rho_soil"//spa, rho_soil           ! densité du sol
-    write(*,*) "rho_snow_fresh"//spa, rho_snow_fresh            ! densité de la neige fraiche
-    write(*,*) "C_water"//spa, C_water          ! Capacité thermique massique de l'eau
-    write(*,*) "C_ice"//spa, C_ice            ! Capacité thermique massique de la glace
-    write(*,*) "C_organic"//spa, C_organic        ! Capacité thermique massique de la matière organique
-    write(*,*) "C_dry_soil"//spa, C_dry_soil        ! Capacité thermique massique du sol
-    write(*,*) "K_other_minerals"//spa, K_other_minerals    ! Conductivité thermique des autres minéraux
-    write(*,*) "K_quartz"//spa, K_quartz            ! conductivité thermique du quartz
-    write(*,*) "K_organic"//spa, K_organic        ! conductivité thermique de la matière organique
-    write(*,*) "K_ice"//spa, K_ice            ! conductivité thermique de la glace
-    write(*,*) "K_fluids"//spa, K_fluids         ! conductivité thermique des fluides
-    write(*,*) "q_quartz"//spa, q_quartz            ! pourcentage de quartz dans le sol
-    write(*,*) "gravity"//spa, gravity          ! accéleration gravitationnelle
-    write(*,*) "Latent_heat"//spa, Latent_heat    ! en J/kg
-    write(*,*) "s_l_max"//spa, s_l_max      ! nombre de couche de neige (marche que avec 1)
-    write(*,*) "gridNoMax"//spa, gridNoMax    !dmr spatial index, no assumption of the spatial arrangement
-    write(*,*) "Tempsolinit"//spa, Tempsolinit
-    write(*,*) "Tempairmonth"//spa, Tempairmonth
-    write(*,*) "Tempairday"//spa, Tempairday
-    write(*,*) "Tempsnowmonth"//spa, Tempsnowmonth
-    write(*,*) "Tempsnowday"//spa, Tempsnowday
+    OPEN (action='write', file=file_path_chck, iostat=rc, newunit=fo)
 
-    write(*,*) "=== ************* ==="
+    write(fo,*) "=== PARAMETERS CHECK ==="
+
+    write(fo,*) "namerun"//spa, namerun
+    write(fo,*) "TotTime"//spa, TotTime         !temps total en année
+    write(fo,*) "nb_day_per_month"//spa, nb_day_per_month           !nombre de jour entre chaque pas de temps
+    write(fo,*) "t_fin"//spa, t_fin
+    write(fo,*) "YearType"//spa, YearType         !nombre de jour par an
+    write(fo,*) "z_num"//spa, z_num            !nombre de couches étudiée
+    write(fo,*) "GridType"//spa, GridType           !(1) Log-generated, (2) Linear-generated
+    write(fo,*) "PorosityType"//spa, PorosityType       !(1) linéaire, (autre) exponentiellement décroissante en fonction de la profondeur
+    write(fo,*) "Bool_Snow"//spa, Bool_Snow          ! forçage en neige ou non (1 ou 0)
+    write(fo,*) "Bool_Organic"//spa, Bool_Organic       ! prise en compte de la couche organique ou non (1 ou 0)
+    write(fo,*) "EQ_Tr"//spa, EQ_Tr             ! Equilibrum run (0) or Transient run (1) -> using different forcing Temperature and snow
+    write(fo,*) "EQ1_EQ2"//spa, EQ1_EQ2           ! EQ1(1) initial temperature calculated with the Geothermal heat flux. EQ2 initial temperature read in a file .txt
+    write(fo,*) "Bool_delta"//spa, Bool_delta        !
+    write(fo,*) "Bool_glacial"//spa, Bool_glacial          ! Using glacial index to modify air temperature
+    write(fo,*) "Bool_layer_temp"//spa, Bool_layer_temp       ! Creation of .txt with the temperature of the soil at different layer
+    write(fo,*) "Bool_Swe_Snw"//spa, Bool_Swe_Snw          ! (1) Snow forcing, (0) Swe forcing
+    write(fo,*) "Bool_Model_Snow"//spa, Bool_Model_Snow       ! (1) Usinsg snow model to find snow_depth, (0) Forcing with snow_depth
+    write(fo,*) "Bool_Bessi"//spa, Bool_Bessi
+    write(fo,*) "Bool_geometric"//spa, Bool_geometric
+    write(fo,*) "Depth"//spa, Depth              !profondeur de la modélisation
+    write(fo,*) "T_init"//spa, T_init                !température initiale a la surface
+    write(fo,*) "T_freeze"//spa, T_freeze             !température où l'eau est considérée comme gelée
+    write(fo,*) "freezing_range"//spa, freezing_range          ! Temperature at wich the snow start to melt
+    write(fo,*) "Gfx"//spa, Gfx                  ! flux géothermique de la terre (a modifier peut être)
+    write(fo,*) "Porosity_soil"//spa, Porosity_soil       ! porosité du sol
+    write(fo,*) "organic_depth"//spa, organic_depth     ! profondeur de la couche organique
+    write(fo,*) "n_organic"//spa, n_organic           ! porosité de la couche organique
+    write(fo,*) "n_soil_bot"//spa, n_soil_bot         ! porosité en bas de la couche de sol
+    write(fo,*) "alpha"//spa, alpha
+    write(fo,*) "rho_snow_freeze"//spa, rho_snow_freeze            ! densité de la neige
+    write(fo,*) "rho_water"//spa, rho_water          ! densité de l'eau
+    write(fo,*) "rho_ice"//spa, rho_ice             ! densité de la glace
+    write(fo,*) "rho_organic"//spa, rho_organic        ! densité de la matière organique
+    write(fo,*) "rho_soil"//spa, rho_soil           ! densité du sol
+    write(fo,*) "rho_snow_fresh"//spa, rho_snow_fresh            ! densité de la neige fraiche
+    write(fo,*) "C_water"//spa, C_water          ! Capacité thermique massique de l'eau
+    write(fo,*) "C_ice"//spa, C_ice            ! Capacité thermique massique de la glace
+    write(fo,*) "C_organic"//spa, C_organic        ! Capacité thermique massique de la matière organique
+    write(fo,*) "C_dry_soil"//spa, C_dry_soil        ! Capacité thermique massique du sol
+    write(fo,*) "K_other_minerals"//spa, K_other_minerals    ! Conductivité thermique des autres minéraux
+    write(fo,*) "K_quartz"//spa, K_quartz            ! conductivité thermique du quartz
+    write(fo,*) "K_organic"//spa, K_organic        ! conductivité thermique de la matière organique
+    write(fo,*) "K_ice"//spa, K_ice            ! conductivité thermique de la glace
+    write(fo,*) "K_fluids"//spa, K_fluids         ! conductivité thermique des fluides
+    write(fo,*) "q_quartz"//spa, q_quartz            ! pourcentage de quartz dans le sol
+    write(fo,*) "gravity"//spa, gravity          ! accéleration gravitationnelle
+    write(fo,*) "Latent_heat"//spa, Latent_heat    ! en J/kg
+    write(fo,*) "s_l_max"//spa, s_l_max      ! nombre de couche de neige (marche que avec 1)
+    write(fo,*) "gridNoMax"//spa, gridNoMax    !dmr spatial index, no assumption of the spatial arrangement
+    write(fo,*) "Tempsolinit"//spa, Tempsolinit
+    write(fo,*) "Tempairmonth"//spa, Tempairmonth
+    write(fo,*) "Tempairday"//spa, Tempairday
+    write(fo,*) "Tempsnowmonth"//spa, Tempsnowmonth
+    write(fo,*) "Tempsnowday"//spa, Tempsnowday
+
+    write(fo,*) "=== ************* ==="
+
+    close(fo)
 
   end subroutine lecture_namelist
 
